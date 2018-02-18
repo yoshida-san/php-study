77383ebd5d42b8d2fd0edf7219ab285964f3dbe7
s-yoshida <yos.3104@gmail.com>
2018-02-18 21:52:44 +0900

    微修正

--- index.php
+++ index.php
@@ -5,16 +5,16 @@ $number = 1;
 
 switch ($number) {
     case 0:
-        echo "$numberは0に等しい";
+        echo '$numberは0に等しい';
         break;
     case 1:
-        echo "$numberは1に等しい";
+        echo '$numberは1に等しい';
         break;
     case 2:
-        echo "$numberは2に等しい";
+        echo '$numberは2に等しい';
         break;
     default:
-       echo "$numberは0,1,2に等しくない";
+       echo '$numberは0,1,2に等しくない';
 }
 
 ?>
