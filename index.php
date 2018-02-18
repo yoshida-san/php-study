82197bba57b08c9b7f3d49e22a6d9fc56ce8c539
s-yoshida <yos.3104@gmail.com>
2018-02-18 21:48:15 +0900

    サンプルコード

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// ここは説明しないよ
+$name = "John Smith";
+
+// もし名前がJohn Smithなら
+if ($name == "John Smith") {
+    // 自己紹介をするよ
+    echo "I'm John Smith :)";
+} else {
+    // John Smithじゃないよ...
+    echo "I'm not John Smith :(";
+}
+
+?>
